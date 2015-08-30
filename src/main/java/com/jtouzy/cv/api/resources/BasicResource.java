a8d4519c399bd368308b4a06bbf7c56750c85a95
Jérémy TOUZY <j.touzy@qualiac.com>
2015-08-30 15:15:37 +0200

    [New] Ressource REST générique

---
+++ src/main/java/com/jtouzy/cv/api/resources/BasicResource.java
@@ -0,0 +1,81 @@
+package com.jtouzy.cv.api.resources;
+
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import com.jtouzy.cv.api.errors.APIException;
+import com.jtouzy.cv.api.errors.ProgramException;
+import com.jtouzy.cv.api.security.RequestSecurityContext;
+import com.jtouzy.dao.DAO;
+import com.jtouzy.dao.DAOManager;
+import com.jtouzy.dao.errors.DAOCrudException;
+import com.jtouzy.dao.errors.DAOInstantiationException;
+import com.jtouzy.dao.errors.NullUniqueIndexException;
+import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.SQLExecutionException;
+
+public class BasicResource<T, D extends DAO<T>> {
+	@Inject
+	protected ContainerRequestContext requestContext;
+	private Class<D> daoClass;
+	
+	public BasicResource(Class<D> daoClass) {
+		this.daoClass = daoClass;
+	}
+
+	@GET
+	public List<T> getAll()
+	throws APIException {
+		try {
+			return DAOManager.getDAO(getRequestContext().getConnection(), daoClass).queryAll();
+		} catch (DAOInstantiationException | QueryException ex) {
+			throw new ProgramException(ex);
+		}
+	}
+	
+	@POST
+	@RolesAllowed("admin")
+	public T create(T object)
+	throws APIException {
+		try {
+			return DAOManager.getDAO(getRequestContext().getConnection(), daoClass).create(object);
+		} catch (DAOInstantiationException | SQLExecutionException | DAOCrudException ex) {
+			throw new ProgramException(ex);
+		}
+	}
+	
+	@PUT
+	@RolesAllowed("admin")
+	public T update(T object)
+	throws APIException {
+		try {
+			return DAOManager.getDAO(getRequestContext().getConnection(), daoClass).update(object);
+		} catch (DAOInstantiationException | SQLExecutionException | DAOCrudException | NullUniqueIndexException ex) {
+			throw new ProgramException(ex);
+		}
+	}
+	
+	@DELETE
+	@RolesAllowed("admin")
+	public void delete(T object)
+	throws APIException {
+		try {
+			DAOManager.getDAO(getRequestContext().getConnection(), daoClass).delete(object);
+		} catch (DAOInstantiationException | SQLExecutionException | DAOCrudException | NullUniqueIndexException ex) {
+			throw new ProgramException(ex);
+		}
+	}
+	
+	protected RequestSecurityContext getRequestContext() {
+		/** Ne pas récupérer directement en injectant le SecurityContext, car Jersey injecte un SecurityContextInjectee */
+		return (RequestSecurityContext)requestContext.getSecurityContext();
+	}
+	
+}
--- src/main/java/com/jtouzy/cv/api/resources/NewsResource.java
+++ src/main/java/com/jtouzy/cv/api/resources/NewsResource.java
@@ -1,36 +1,14 @@
 package com.jtouzy.cv.api.resources;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.ws.rs.GET;
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import com.jtouzy.cv.api.config.AppConfig;
-import com.jtouzy.cv.api.errors.APIException;
-import com.jtouzy.cv.api.errors.ProgramException;
 import com.jtouzy.cv.model.classes.News;
 import com.jtouzy.cv.model.dao.NewsDAO;
-import com.jtouzy.dao.DAOManager;
-import com.jtouzy.dao.errors.DAOInstantiationException;
-import com.jtouzy.dao.errors.QueryException; 
 
 @Path("/news")
-public class NewsResource {
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<News> getAll()
-	throws APIException {
-		try {
-			Connection connection = AppConfig.getDataSource().getConnection();
-			List<News> news = DAOManager.getDAO(connection, NewsDAO.class).queryAll();
-			connection.close();
-			return news;
-		} catch (SQLException | DAOInstantiationException | QueryException ex) {
-			throw new ProgramException(ex);
-		}
+@PermitAll
+public class NewsResource extends BasicResource<News, NewsDAO> {
+	public NewsResource() {
+		super(NewsDAO.class);
 	}
 }
