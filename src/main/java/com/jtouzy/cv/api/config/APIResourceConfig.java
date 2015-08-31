8b7105534ce113f54870c3828c8f95b5b8764f28
Jérémy TOUZY <j.touzy@qualiac.com>
2015-08-31 22:41:34 +0200

    [New] Définition des jersey-features au détail dans l'application

--- src/main/java/com/jtouzy/cv/api/config/APIResourceConfig.java
+++ src/main/java/com/jtouzy/cv/api/config/APIResourceConfig.java
@@ -3,11 +3,21 @@ package com.jtouzy.cv.api.config;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
+import com.jtouzy.cv.api.errors.mappers.APIExceptionMapper;
+import com.jtouzy.cv.api.errors.mappers.BeanValidationExceptionMapper;
+import com.jtouzy.cv.api.filters.AuthFilter;
 import com.jtouzy.cv.api.lifecycle.APIEventListener;
 
 public class APIResourceConfig extends ResourceConfig {
 	public APIResourceConfig() {
+		// Fonctionnalités
 		register(RolesAllowedDynamicFeature.class);
+		// Ecouteurs
 		register(APIEventListener.class);
+		// Filtres
+		register(AuthFilter.class);
+		// Transformations d'exceptions
+		register(APIExceptionMapper.class);
+		register(BeanValidationExceptionMapper.class);
 	}
 }
