734a10c962ce12c3e9c9b291b70ee8f3eecdc58e
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-01 17:56:37 +0200

    [New] Initialisation des données en cache au démarrage de la servlet

--- src/main/java/com/jtouzy/cv/api/APIServlet.java
+++ src/main/java/com/jtouzy/cv/api/APIServlet.java
@@ -1,12 +1,19 @@
 package com.jtouzy.cv.api;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 
 import org.glassfish.jersey.servlet.ServletContainer;
 
 import com.jtouzy.cv.api.config.AppConfig;
 import com.jtouzy.cv.api.errors.APIConfigurationException;
+import com.jtouzy.cv.api.errors.CacheLoadException;
+import com.jtouzy.cv.model.dao.SeasonDAO;
 import com.jtouzy.dao.DAOManager;
+import com.jtouzy.dao.errors.DAOInstantiationException;
+import com.jtouzy.dao.errors.QueryException;
 import com.jtouzy.dao.errors.model.ModelClassDefinitionException;
 
 /**
@@ -28,8 +35,26 @@ public class APIServlet extends ServletContainer {
 		try {
 			AppConfig.init();
 			DAOManager.init("com.jtouzy.cv.model.classes");
+			loadCachedData();
 		} catch (ModelClassDefinitionException | APIConfigurationException ex) {
 			throw new ServletException(ex);
+		}
+	}
+	
+	/**
+	 * Chargement des données à mettre en cache au démarrage du serveur
+	 * <ul>
+	 * 	<li>Chargement de la saison courante dans le SeasonDAO</li>
+	 * </ul>
+	 * @throws CacheLoadException lorsqu'une exception survient au chargement du cache
+	 */
+	private void loadCachedData()
+	throws CacheLoadException {
+		try (Connection loadDataCacheConnection = AppConfig.getDataSource().getConnection()) {
+			SeasonDAO seasonDao = DAOManager.getDAO(loadDataCacheConnection, SeasonDAO.class);
+			seasonDao.getCurrentSeason();
+		} catch (SQLException | DAOInstantiationException | QueryException ex) {
+			throw new CacheLoadException(ex);
 		}
 	}
 }
---
+++ src/main/java/com/jtouzy/cv/api/errors/CacheLoadException.java
@@ -0,0 +1,9 @@
+package com.jtouzy.cv.api.errors;
+
+public class CacheLoadException extends ProgramException {
+	private static final long serialVersionUID = 1L;
+
+	public CacheLoadException(Throwable cause) {
+		super(cause);
+	}
+}
