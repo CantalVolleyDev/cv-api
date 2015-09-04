e908641342b2471e38f2687a4141207ec82c960a
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-04 22:26:42 +0200

    [New] Paramètre "published" + Tri des actualités par date DESC

--- src/main/java/com/jtouzy/cv/api/resources/NewsResource.java
+++ src/main/java/com/jtouzy/cv/api/resources/NewsResource.java
@@ -2,13 +2,41 @@ package com.jtouzy.cv.api.resources;
 
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
 import com.jtouzy.cv.model.classes.News;
 import com.jtouzy.cv.model.dao.NewsDAO;
+import com.jtouzy.dao.errors.DAOInstantiationException;
+import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.model.ColumnContextNotFoundException;
+import com.jtouzy.dao.query.Query;
 
 @Path("/news")
 @PermitAll
 public class NewsResource extends BasicResource<News, NewsDAO> {
+	@QueryParam("published")
+	protected Boolean published;
+	
 	public NewsResource() {
 		super(NewsDAO.class);
+	}
+	
+	@Override
+	public Query<News> query()
+	throws DAOInstantiationException, QueryException {
+		try {
+			Query<News> query = super.query();
+			if (published != null) {
+				News.State state = News.State.C;
+				if (published) {
+					state = News.State.V;
+				}
+				query.context().addEqualsCriterion(News.STATE_FIELD, state);
+			}
+			query.context().orderBy(News.PUBLISH_DATE_FIELD, false);
+			return query;
+		} catch (ColumnContextNotFoundException ex) {
+			throw new QueryException(ex);
+		}
 	}
 }
