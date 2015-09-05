55d814266de73bd9373df56602aedcdea185f226
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-05 16:11:04 +0200

    [Fix] Ajout du nom du driver JDBC pour tomcat non-embarqués

--- src/main/java/com/jtouzy/cv/api/config/AppConfig.java
+++ src/main/java/com/jtouzy/cv/api/config/AppConfig.java
@@ -15,6 +15,7 @@ public class AppConfig {
 	public static final String DB_PUBLIC_PASSWORD_PROPERTY = "db.public.password";
 	public static final String DB_DATABASE_PROPERTY = "db.databaseName";
 	public static final String DB_JDBCURL_PROPERTY = "db.jdbcUrl";
+	public static final String DB_DRIVER_CLASSNAME = "db.driverClassName";
 	public static final String DROPBOX_APP_CLIENT_TOKEN = "dbx.client.token";
 	public static final String DROPBOX_APP_NAME = "dbx.appname";
 	public static final String ORIGIN_ALLOWED = "origin.allowed";
@@ -56,6 +57,7 @@ public class AppConfig {
 		                   .toString());
 		config.setUsername(properties.getProperty(DB_ADMIN_USER_PROPERTY));
 		config.setPassword(properties.getProperty(DB_ADMIN_PASSWORD_PROPERTY));
+		config.setDriverClassName(properties.getProperty(DB_DRIVER_CLASSNAME));
 		config.addDataSourceProperty("cachePrepStmts", "true");
 		config.addDataSourceProperty("prepStmtCacheSize", "250");
 		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
