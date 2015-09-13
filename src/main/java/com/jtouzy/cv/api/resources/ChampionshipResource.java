59b2775b9515eed628de13b6e8f30adc372a8150
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-13 21:13:49 +0200

    [New] Ressource "championships" avec prévisualisation HTML

---
+++ src/main/java/com/jtouzy/cv/api/resources/ChampionshipResource.java
@@ -0,0 +1,62 @@
+package com.jtouzy.cv.api.resources;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.jtouzy.cv.model.classes.Championship;
+import com.jtouzy.cv.model.classes.Match;
+import com.jtouzy.cv.model.dao.ChampionshipDAO;
+import com.jtouzy.cv.model.dao.MatchDAO;
+import com.jtouzy.cv.model.errors.CalendarGenerationException;
+import com.jtouzy.dao.DAOManager;
+import com.jtouzy.dao.errors.DAOInstantiationException;
+
+@Path("/championships")
+public class ChampionshipResource extends BasicResource<Championship, ChampionshipDAO> {
+	public ChampionshipResource() {
+		super(Championship.class, ChampionshipDAO.class);
+	}
+	
+	@GET
+	@Path("/{id}/previewCalendar")
+	@Produces(MediaType.TEXT_HTML)
+	public String previewCalendar(@PathParam("id") Integer championshipId)
+	throws CalendarGenerationException, DAOInstantiationException {
+		final StringBuilder text = new StringBuilder();
+		text.append("<html><head><title>Prévisualisation</title></head><body>");
+		MatchDAO dao = DAOManager.getDAO(getRequestContext().getConnection(), MatchDAO.class);
+		List<Match> matchs = dao.buildCalendar(championshipId, false);
+		
+		Map<Integer, List<Match>> matchsByStep = matchs.stream()
+				                                       .collect(Collectors.groupingBy(m -> m.getStep()));
+		Iterator<Map.Entry<Integer, List<Match>>> it = matchsByStep.entrySet().iterator();
+		Map.Entry<Integer, List<Match>> entry;
+		Integer step;
+		while (it.hasNext()) {
+			entry = it.next();
+			step = entry.getKey();
+			text.append("<strong>")
+			    .append("Journée ")
+			    .append(step)
+			    .append("</strong><br>");
+			entry.getValue().forEach(m -> {
+				text.append(m.getFirstTeam().getLabel())
+				    .append(" - ")
+				    .append(m.getSecondTeam().getLabel())
+				    .append("<br>");
+			});
+			if (it.hasNext())
+				text.append("<br>");
+		}
+		text.append("</body></html>");
+		return text.toString();
+	}
+}
