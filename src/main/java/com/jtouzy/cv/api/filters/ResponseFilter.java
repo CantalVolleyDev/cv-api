7b436b0442762d21d2f6d136b8cce607c681c431
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-17 19:02:54 +0200

    [Fix] 'Allow-Origin' : Correction des autorisations multiples

--- src/main/java/com/jtouzy/cv/api/filters/ResponseFilter.java
+++ src/main/java/com/jtouzy/cv/api/filters/ResponseFilter.java
@@ -3,12 +3,15 @@ package com.jtouzy.cv.api.filters;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+import com.google.common.base.Splitter;
 import com.jtouzy.cv.api.config.AppConfig;
 import com.jtouzy.cv.api.security.RequestSecurityContext;
 import com.jtouzy.dao.DAOManager;
@@ -20,7 +23,20 @@ public class ResponseFilter implements ContainerResponseFilter {
 	throws IOException {
 		String property = AppConfig.getProperty(AppConfig.ORIGIN_ALLOWED);
 		if (property != null) {
-			responseContext.getHeaders().add("Access-Control-Allow-Origin", property);
+			if (property.contains(",")) {
+				List<String> origins = requestContext.getHeaders().get("Origin");
+				if (origins != null && origins.size() > 0) {
+					String origin = origins.get(0);
+					List<String> authorized = Splitter.on(',').omitEmptyStrings().splitToList(property);
+					Optional<String> opt = authorized.stream().filter(d -> d.equals(origin)).findFirst();
+					if (opt.isPresent()) {
+						responseContext.getHeaders().add("Access-Control-Allow-Origin", opt.get());
+					}
+				}
+				
+			} else {
+				responseContext.getHeaders().add("Access-Control-Allow-Origin", property);
+			}
 		}
 		try {
 			RequestSecurityContext securityContext = (RequestSecurityContext)requestContext.getSecurityContext();
