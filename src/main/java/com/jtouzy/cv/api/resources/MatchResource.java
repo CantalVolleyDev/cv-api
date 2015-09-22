74f3b2638a80701e4f57e1e329ae455a7ffa6687
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-22 14:34:29 +0200

    [New] Ajout GET /matchs/id/submitInfos

--- src/main/java/com/jtouzy/cv/api/resources/MatchResource.java
+++ src/main/java/com/jtouzy/cv/api/resources/MatchResource.java
@@ -1,13 +1,23 @@
 package com.jtouzy.cv.api.resources;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import com.jtouzy.cv.api.errors.APIException;
+import com.jtouzy.cv.api.errors.ProgramException;
 import com.jtouzy.cv.model.classes.Match;
+import com.jtouzy.cv.model.classes.MatchPlayer;
+import com.jtouzy.cv.model.classes.SeasonTeamPlayer;
 import com.jtouzy.cv.model.dao.MatchDAO;
+import com.jtouzy.cv.model.dao.MatchPlayerDAO;
+import com.jtouzy.cv.model.dao.SeasonTeamPlayerDAO;
+import com.jtouzy.cv.model.errors.MatchNotFoundException;
 import com.jtouzy.cv.model.errors.UserNotFoundException;
 import com.jtouzy.dao.errors.DAOInstantiationException;
 import com.jtouzy.dao.errors.QueryException;
@@ -18,6 +28,42 @@ public class MatchResource extends BasicResource<Match, MatchDAO> {
 		super(Match.class, MatchDAO.class);
 	}
 
+	@GET
+	@Path("/{id}/submitInfos")
+	public MatchSubmitInfos getMatchSubmitInfos(@PathParam("id") Integer matchId)
+	throws MatchNotFoundException, ProgramException {
+		try {
+			// Lecture du match avec détails (Exception si le match n'existe pas)
+			Match match = getDAO().queryOneWithDetails(matchId);
+			// Select unique pour récupérer tous les joueurs des 2 équipes
+			// On fait ensuite un tri pour diviser, à la place de faire 2 select
+			List<SeasonTeamPlayer> allPlayers = 
+					getDAO(SeasonTeamPlayerDAO.class).getPlayers(
+							match.getChampionship().getCompetition().getSeason().getIdentifier(), 
+							Arrays.asList(match.getFirstTeam().getIdentifier(), 
+									      match.getSecondTeam().getIdentifier()));
+			List<MatchPlayer> matchPlayers = getDAO(MatchPlayerDAO.class).getPlayers(matchId);
+			// Création des informations du match
+			MatchSubmitInfos infos = new MatchSubmitInfos();
+			infos.setMatch(match);
+			infos.setFirstTeamPlayers(allPlayers.stream()
+					                            .filter(s -> s.getTeam().getIdentifier() == match.getFirstTeam().getIdentifier())
+					                            .collect(Collectors.toList()));
+			infos.setSecondTeamPlayers(allPlayers.stream()
+					                             .filter(s -> s.getTeam().getIdentifier() == match.getSecondTeam().getIdentifier())
+					                             .collect(Collectors.toList()));
+			infos.setFirstTeamMatchPlayers(matchPlayers.stream()
+					                                   .filter(p -> p.getTeam().getIdentifier() == match.getFirstTeam().getIdentifier())
+					                                   .collect(Collectors.toList()));
+			infos.setSecondTeamMatchPlayers(matchPlayers.stream()
+                                                        .filter(p -> p.getTeam().getIdentifier() == match.getSecondTeam().getIdentifier())
+                                                        .collect(Collectors.toList()));
+			return infos;
+		} catch (DAOInstantiationException | QueryException ex) {
+			throw new ProgramException(ex);
+		}
+	}
+	
 	@Override
 	public List<Match> getAll() 
 	throws APIException {
---
+++ src/main/java/com/jtouzy/cv/api/resources/MatchSubmitInfos.java
@@ -0,0 +1,46 @@
+package com.jtouzy.cv.api.resources;
+
+import java.util.List;
+
+import com.jtouzy.cv.model.classes.Match;
+import com.jtouzy.cv.model.classes.MatchPlayer;
+import com.jtouzy.cv.model.classes.SeasonTeamPlayer;
+
+public class MatchSubmitInfos {
+	private Match match;
+	private List<SeasonTeamPlayer> firstTeamPlayers;
+	private List<SeasonTeamPlayer> secondTeamPlayers;
+	private List<MatchPlayer> firstTeamMatchPlayers;
+	private List<MatchPlayer> secondTeamMatchPlayers;
+
+	public Match getMatch() {
+		return match;
+	}
+	public void setMatch(Match match) {
+		this.match = match;
+	}
+	public List<SeasonTeamPlayer> getFirstTeamPlayers() {
+		return firstTeamPlayers;
+	}
+	public void setFirstTeamPlayers(List<SeasonTeamPlayer> firstTeamPlayers) {
+		this.firstTeamPlayers = firstTeamPlayers;
+	}
+	public List<SeasonTeamPlayer> getSecondTeamPlayers() {
+		return secondTeamPlayers;
+	}
+	public void setSecondTeamPlayers(List<SeasonTeamPlayer> secondTeamPlayers) {
+		this.secondTeamPlayers = secondTeamPlayers;
+	}
+	public List<MatchPlayer> getFirstTeamMatchPlayers() {
+		return firstTeamMatchPlayers;
+	}
+	public void setFirstTeamMatchPlayers(List<MatchPlayer> firstTeamMatchPlayers) {
+		this.firstTeamMatchPlayers = firstTeamMatchPlayers;
+	}
+	public List<MatchPlayer> getSecondTeamMatchPlayers() {
+		return secondTeamMatchPlayers;
+	}
+	public void setSecondTeamMatchPlayers(List<MatchPlayer> secondTeamMatchPlayers) {
+		this.secondTeamMatchPlayers = secondTeamMatchPlayers;
+	}
+}
