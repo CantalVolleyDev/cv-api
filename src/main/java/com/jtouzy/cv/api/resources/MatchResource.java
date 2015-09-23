6af1c52476ae24a621cca306616e8d3dac2ff3fa
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-23 23:01:51 +0200

    [New] Ajout /matchs/id/submit

--- src/main/java/com/jtouzy/cv/api/resources/MatchResource.java
+++ src/main/java/com/jtouzy/cv/api/resources/MatchResource.java
@@ -5,8 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -20,10 +23,11 @@ import com.jtouzy.cv.model.classes.SeasonTeamPlayer;
 import com.jtouzy.cv.model.dao.MatchDAO;
 import com.jtouzy.cv.model.dao.MatchPlayerDAO;
 import com.jtouzy.cv.model.dao.SeasonTeamPlayerDAO;
-import com.jtouzy.cv.model.errors.MatchNotFoundException;
 import com.jtouzy.cv.model.errors.UserNotFoundException;
+import com.jtouzy.dao.errors.DAOCrudException;
 import com.jtouzy.dao.errors.DAOInstantiationException;
 import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.validation.DataValidationException;
 
 @Path("/matchs")
 public class MatchResource extends BasicResource<Match, MatchDAO> {
@@ -35,10 +39,16 @@ public class MatchResource extends BasicResource<Match, MatchDAO> {
 	@Path("/{id}/submitInfos")
 	@RolesAllowed(Roles.CONNECTED)
 	public MatchSubmitInfos getMatchSubmitInfos(@PathParam("id") Integer matchId)
-	throws MatchNotFoundException, ProgramException, NotAuthorizedException {
+	throws ProgramException, NotAuthorizedException, NotFoundException {
 		try {
 			// Lecture du match avec détails (Exception si le match n'existe pas)
 			Match match = getDAO().queryOneWithDetails(matchId);
+			if (match == null) {
+				throw new NotFoundException("Match " + matchId + " non trouvé");
+			}
+			if (match.getState() == Match.State.V) {
+				throw new NotAuthorizedException("Match " + matchId + " déjà validé", "");
+			}
 			// Select unique pour récupérer tous les joueurs des 2 équipes
 			// On fait ensuite un tri pour diviser, à la place de faire 2 select
 			List<SeasonTeamPlayer> allPlayers = 
@@ -77,6 +87,32 @@ public class MatchResource extends BasicResource<Match, MatchDAO> {
 					                          .collect(Collectors.toList()));
 			return infos;
 		} catch (DAOInstantiationException | QueryException ex) {
+			throw new ProgramException(ex);
+		}
+	}
+	
+	@POST
+	@Path("/{id}/submit")
+	@RolesAllowed(Roles.CONNECTED)
+	public void submit(@PathParam("id") Integer matchId, MatchSubmitInfos submit)
+	throws ProgramException, DataValidationException {
+		try {
+			if (submit == null) {
+				throw new BadRequestException("Paramètre pour les informations du match absent");
+			}
+			// Lecture des informations du match pour vérifier la cohérence
+			Match match = getDAO().queryForOne(matchId);
+			if (match == null) {
+				throw new NotFoundException("Match " + matchId + " non trouvé");
+			}
+			if (match.getState() == Match.State.V) {
+				throw new NotAuthorizedException("Le match " + matchId + " est déjà validé", "");
+			}
+			// Match soumis par la requête
+			Match submitted = submit.getMatch();
+			getDAO().update(submitted);
+			// FIXME: DAOCRUD progamexception, DATAVALIDATIONA LEVER NATUREL
+		} catch (DAOInstantiationException | QueryException | DAOCrudException ex) {
 			throw new ProgramException(ex);
 		}
 	}
