3abfeb608e555e0b8ecae718d7e9cf16a36bbfc2
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-23 23:00:12 +0200

    [New] Gestion mapper des exceptions 400 et 404

--- src/main/java/com/jtouzy/cv/api/errors/mappers/APIExceptionMapper.java
+++ src/main/java/com/jtouzy/cv/api/errors/mappers/APIExceptionMapper.java
@@ -1,6 +1,8 @@
 package com.jtouzy.cv.api.errors.mappers;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -19,12 +21,22 @@ public class APIExceptionMapper implements ExceptionMapper<Exception> {
 						       .type(MediaType.APPLICATION_JSON)
 						       .entity(new ExceptionDescriptor(exception, build401Message(exception)))
 						       .build();
+			} else if (exception instanceof NotFoundException) {
+				return Response.status(Response.Status.NOT_FOUND)
+						       .type(MediaType.APPLICATION_JSON)
+						       .entity(new ExceptionDescriptor(exception, build404Message(exception)))
+						       .build();
+			} else if (exception instanceof BadRequestException) {
+				return Response.status(Response.Status.BAD_REQUEST)
+						       .type(MediaType.APPLICATION_JSON)
+						       .entity(new ExceptionDescriptor(exception, build400Message(exception)))
+						       .build();
 			}
 			return ((WebApplicationException)exception).getResponse();
 		} else {
 			return Response.status(500)
 					       .type(MediaType.APPLICATION_JSON)
-					       .entity(new ExceptionDescriptor(exception))
+					       .entity(new ExceptionDescriptor(exception, exception.getMessage()))
 					       .build();
 		}
 	}
@@ -32,6 +44,18 @@ public class APIExceptionMapper implements ExceptionMapper<Exception> {
 	private String build401Message(Exception exception) {
 		if (exception.getMessage().startsWith("HTTP"))
 			return "Vous devez être connecté pour accéder à cette page";
+		return exception.getMessage();
+	}
+	
+	private String build404Message(Exception exception) {
+		if (exception.getMessage().startsWith("HTTP"))
+			return "La page que vous demandez n'existe pas";
+		return exception.getMessage();
+	}
+	
+	private String build400Message(Exception exception) {
+		if (exception.getMessage().startsWith("HTTP"))
+			return "La requête pour la page demandée est incorrecte";
 		return exception.getMessage();
 	}
 }
